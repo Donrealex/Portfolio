26d2283e88535a8e272cdd37b6df9cea020cce76
Adebiyi Ridwan <donrealex11@gmail.com>
2025-09-03 10:56:07 +0100

    feat:add skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { BrowserRouter, Router, Routes } from 'react-router-dom'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Skills from './components/Skills'
 
 const App = () => {
   return (
-    <>
+  
     <BrowserRouter>
         <Navbar />
         <Hero />
+        <Skills />
     </BrowserRouter>
-    </>
+   
    
      
   )
---
+++ src/components/Skills.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Skills = () => {
+     const skills = [
+       "JavaScript",
+       "React",
+       "Node.js",
+       "TailwindCSS",
+       "Git",
+     ];
+  return (
+    <section className="bg-[#0f172a] text-white py-16 px-6">
+      {/* Title */}
+      <h2 className="text-2xl sm:text-3xl font-bold text-center mb-10">
+        Skills & Technologies
+      </h2>
+
+      {/* Skills Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-4xl mx-auto">
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            className="bg-[#1e293b] rounded-lg shadow-lg py-6 px-4 flex items-center justify-center text-center text-lg font-medium hover:border-t-2 hover:text-black scale-100 hover:scale-110 transition-transform duration-500"
+          >
+            {skill}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Skills
